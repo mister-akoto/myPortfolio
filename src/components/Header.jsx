f353d6fe6fe9331fcaa2de87d92f9a8497632b13
Akoto Etornam Tutu <etornamtutuakoto@gmail.com>
2026-01-04 09:52:38 +0000

    feat: Implement theme toggle at Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,39 @@
-import { Sun } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
 import React from 'react'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  
+  function toggleTheme(){
+    setIsDarkMode(!isDarkMode)
+    if(!isDarkMode){
+      document.documentElement.classList.add('dark')
+    }else{
+      document.documentElement.classList.remove('dark')
+    }
+  }
+  
+  
+
   return (
-    <header className='bg-red-600 flex justify-between items-center px-6 py-2 backdrop-blur-md'>
-      <h1 className='text-2xl font-bold'>Akoto Etornam</h1>
+    <header className='flex justify-between items-center px-6 py-2 backdrop-blur-md '>
+      <h1 className='text-2xl font-bold text-primary'>Akoto Etornam</h1>
 
       <div className='flex gap-8' >
-        <nav className='text-lg'>
-          <ul className='flex items-center space-x-4'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+        <nav className='text-lg text-primary ' >
+          <ul className='flex items-center space-x-4  '>
+            <li className='hover:text-foreground'><a href="#hero">Home</a></li>
+            <li className='hover:text-foreground'><a href="about">About</a></li>
+            <li className='hover:text-foreground'><a href="skills">Skills</a></li>
+            <li className='hover:text-foreground'><a href="projects">Projects</a></li>
+            <li className='hover:text-foreground'><a href="contact">Contact</a></li>
           </ul>
         </nav>
-        <Sun size={24}/>
+       {  isDarkMode?
+         <Sun size={24} className='text-yellow-300 cursor-pointer' onClick={toggleTheme}/>:
+         <Moon size={24} className='cursor-pointer text-blue-900' onClick={toggleTheme}/>
+       }
       </div>
       
     </header>
